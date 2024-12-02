256b976db79eb3f8592dda51360f27e44ecd3117
ainsley78 <160363892+ainsley78@users.noreply.github.com>
2024-12-01 22:14:26 -0600

    Update q2.cpp

--- Solutions/q2.cpp
+++ Solutions/q2.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+string replaceFourLetterWords(const string& input) {
+    stringstream ss(input), result;
+    string word;
+
+    while (ss >> word) {
+        if (word.length() == 4) { // Check if word length is 4
+            if (isupper(word[0])) // Check if first letter is uppercase
+                result << "Love ";
+            else
+                result << "love ";
+        } else {
+            result << word << " ";
+        }
+    }
+
+    string output = result.str();
+    return output.substr(0, output.length() - 1); // Remove trailing space
+}
+
+int main() {
+    string input;
+    cout << "Enter a line of text: ";
+    getline(cin, input);
+
+    string modified = replaceFourLetterWords(input);
+    cout << "Modified Line: " << modified << endl;
+    return 0;
+}
 
